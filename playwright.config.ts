fe86ea7fc72ab315e0b4fb7e680859904c73371c
Italo Renan <italorenan290898@gmail.com>
2026-05-17 18:51:27 -0300

    Add CI pipeline for UI and API tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,13 +9,14 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [
-    ['html'],
-    ['list']
+    ['list'],
+    ['html', { outputFolder: 'evidencias/ui/reports/html', open: 'never' }],
+    ['json', { outputFile: 'evidencias/ui/reports/json/ui-report.json' }]
   ],
   use: {
     baseURL: 'https://www.saucedemo.com',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure'
+    video: 'retain-on-failure',
   }
 });
